7b53d95391b7c1519d891f76dec845eb5c6d04bb
nbnbnb-coder <mitalebi2009@gmail.com>
2026-06-20 18:15:42 +0330

    اصلاح main.py

--- main.py
+++ main.py
@@ -1,25 +1,30 @@
-from fastapi import FastAPI, WebSocket, WebSocketDisconnect
+from fastapi import FastAPI,WebSocket,WebSocketDisconnect
+from routes import router
+from websocket_manager import manager
+import json
 
-app = FastAPI()
+app=FastAPI()
 
-connections = {}
+app.include_router(router)
 
-@app.websocket("/ws/{username}")
-async def websocket_endpoint(websocket: WebSocket, username: str):
-    await websocket.accept()
-    connections[username] = websocket
+@app.websocket("/ws/{user_id}")
+async def websocket(ws:WebSocket,user_id:int):
+
+    await manager.connect(user_id,ws)
 
     try:
         while True:
-            data = await websocket.receive_json()
-            to_user = data["to"]
-            message = data["message"]
 
-            if to_user in connections:
-                await connections[to_user].send_json({
-                    "from": username,
-                    "message": message
-                })
+            data=await ws.receive_text()
+            data=json.loads(data)
+
+            to=data["to"]
+
+            await manager.send_private(to,{
+                "from":user_id,
+                "message":data["message"],
+                "type":data["type"]
+            })
 
     except WebSocketDisconnect:
-        connections.pop(username, None)
+        manager.disconnect(user_id)
