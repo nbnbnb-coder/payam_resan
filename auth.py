43f387acc9065ad081085996ca83bcc1446e541b
nbnbnb-coder <mitalebi2009@gmail.com>
2026-06-21 18:03:05 +0330

    حل مشکل چهارم auth.py

--- auth.py
+++ auth.py
@@ -1,32 +1,27 @@
-import os
-from datetime import datetime, timedelta
-
-from jose import jwt
 from passlib.context import CryptContext
+from jose import jwt
+from datetime import datetime, timedelta
+import os
 
-
-SECRET_KEY = os.getenv("SECRET_KEY", "CHANGE_ME")
+SECRET_KEY = os.getenv("SECRET_KEY", "CHANGE_THIS_SECRET_KEY")
 ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_DAYS = 7
+ACCESS_TOKEN_EXPIRE_MINUTES = 60 * 24
 
 pwd_context = CryptContext(
     schemes=["bcrypt_sha256"],
     deprecated="auto"
 )
 
-
-def hash_password(password: str) -> str:
+def hash_password(password: str):
+    print("AUTH HASH USING:", pwd_context.schemes())
     return pwd_context.hash(password)
 
-
-def verify_password(plain_password: str, hashed_password: str) -> bool:
+def verify_password(plain_password: str, hashed_password: str):
+    print("AUTH VERIFY USING:", pwd_context.schemes())
     return pwd_context.verify(plain_password, hashed_password)
 
-
-def create_token(data: dict) -> str:
+def create_access_token(data: dict):
     to_encode = data.copy()
-
-    expire = datetime.utcnow() + timedelta(days=ACCESS_TOKEN_EXPIRE_DAYS)
+    expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
     to_encode.update({"exp": expire})
-
     return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
